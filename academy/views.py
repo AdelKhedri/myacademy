aa24f045b1621986fa5bb7774cb6a4e4e74bd4e1
adel <adel.khedri.mirqaid@gmail.com>
2025-02-23 21:18:41 +0330

    change login view and add tests

--- academy/views.py
+++ academy/views.py
@@ -1,4 +1,3 @@
-from django.forms import ValidationError
 from django.shortcuts import render, redirect
 from django.views.generic import View
 from .forms import RecaptchaFrom, RegisterForm
@@ -7,12 +6,14 @@ from .forms import RecaptchaFrom, RegisterForm
 class RegisterView(View):
     template_name = 'academy/register.html'
 
-    def setup(self, request, *args, **kwargs):
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('academy:home')
         self.context = {
             'recaptcha': RecaptchaFrom(),
             'register_form': RegisterForm()
         }
-        return super().setup(request, *args, **kwargs)
+        return super().dispatch(request, *args, **kwargs)
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name, self.context)
@@ -23,10 +24,12 @@ class RegisterView(View):
             register_form = RegisterForm(request.POST)
             if register_form.is_valid():
                 register_form.save()
+                request.session['phone_number'] = register_form.cleaned_data['phone_number']
+                request.session.modified = True
                 res = register_form.send_otp_code_to_number()
                 if not res:
                     self.context['msg'] = 'otp connection failed'
-                return redirect('academi:confirm-account')
+                # return redirect('academi:confirm-account')
             else:
                 self.context['register_form'] = register_form
                 self.context['msg'] = 'register form error'
