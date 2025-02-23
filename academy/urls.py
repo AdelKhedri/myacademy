3b95f404114c43bc359f5555b8e4dc2a2dc47b68
adel <adel.khedri.mirqaid@gmail.com>
2025-02-23 21:29:27 +0330

    add login view

--- academy/urls.py
+++ academy/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RegisterView, ActivateRegisterdAccountView, LoginView, Home
+from .views import RegisterView, LoginView, Home
 
 
 app_name = 'academy'
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
     path('', Home, name = 'home')
 ]
--- academy/views.py
+++ academy/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from .forms import RecaptchaFrom, RegisterForm
+from .forms import RecaptchaFrom, RegisterForm, LoginForm
+from django.contrib.auth import login, authenticate
+from django.http import HttpResponse
 
 
 class RegisterView(View):
@@ -36,3 +38,42 @@ class RegisterView(View):
         else:
             self.context['msg'] = 'captcha error'
         return render(request, self.template_name, self.context)
+
+
+class LoginView(View):
+    template_name = 'academy/login.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('academy:home')
+
+        self.context = {
+            'login_form': LoginForm(),
+            'recaptcha': RecaptchaFrom(),
+        }
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, self.context)
+
+    def post(self, request, *args, **kwargs):
+        recaptcha_form = RecaptchaFrom(request.POST)
+        if recaptcha_form.is_valid():
+            login_form = LoginForm(request.POST)
+            if login_form.is_valid():
+                # username = login_form.cleaned_data['username']
+                # password = login_form.cleaned_data['password']
+                user = authenticate(request, **login_form.cleaned_data)
+                if user:
+                    login(request, user)
+                    return redirect(request.GET.get('next', 'academy:home'))
+                self.context['msg'] = 'authentication failed'
+            self.context['login_form'] = login_form
+        else:
+            self.context['msg'] = 'captcha error'
+        return render(request, self.template_name, self.context)
+
+
+def Home(request):
+    return HttpResponse('ss')
+
